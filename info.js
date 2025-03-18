dcd5005b645e4814ff38cb20c1ad2b74c9dc94ca
PromitSarker <58898021+PromitSarker@users.noreply.github.com>
2025-03-19 00:04:05 +0600

    Add files via upload

---
+++ info.js
@@ -0,0 +1,241 @@
+let info = {
+  name: "Promit Sarker",
+  logo_name: "PS",
+  //flat_picture: require("./src/assets/potrait.jpg"),
+  config: {
+    use_cookies: true,
+    navbar: {
+      blur: false,
+    },
+  },
+  description:
+    "Motivated Computer Science student with a strong grasp of popular programming languages and algorithms. Also a Linux enthusiast with a keen interest in artificial intelligence (AI) and Quantum Machine Learning. Actively engaged in research and highly enthusiastic about development and Machine Learning.",
+  links: {
+    linkedin: "http://www.linkedin.com/in/promitsarker",
+    github: "https://github.com/PromitSarker",
+    resume:
+      "https://drive.google.com/file/d/1Ds81PPlCjIofp6_6fxhjueqTU5AN-QaT/view?usp=sharing",
+  },
+  education: [
+    {
+      name: "North South University",
+      place: "Dhaka, Bangladesh",
+      date: "2020 - 2025",
+      degree: "Bachelors in Computer Science and Engineering",
+    },
+    {
+      name: "Notre Dame College, Mymensingh",
+      place: "Mymensingh, Bangladesh",
+      date: "2018 - 2019",
+      degree: "Higher Secondary Certificate",
+    },
+  ],
+  experience: [
+    {
+      name: "NSU HCI Design Inclusion and Access Lab",
+      place: "Dhaka, Bangladesh",
+      date: "Feb, 2023 - June 2023",
+      position: "Research Assistant",
+    },
+    {
+      name: "IEEE NSU Student Branch",
+      place: "Dhaka, Bangladesh",
+      date: "2020 - 2021",
+      position: "Member",
+    },
+    {
+      name: "Spellbound - স্পেলবাউন্ড",
+      place: "Dhaka, Bangladesh",
+      date: "2020 - 2022",
+      position: "Editorial Member",
+    },
+    {
+      name: "Manabu Bookstore",
+      place: "Dhaka, Bangladesh",
+      date: "2020 - 2022",
+      position: "Co-Owner",
+    },
+    {
+      name: "Notre Dame Science Club",
+      place: "Mymensingh, Bangladesh",
+      date: "2018 - 2019",
+      position: "President of Administration",
+    },
+  ],
+  skills: [
+    {
+      title: "languages",
+      info: ["Python", "Javascript", "Java", "C", "C++", "SQL"],
+      icon: "fa fa-code",
+    },
+    {
+      title: "data frameworks",
+      info: [
+        "Qiskit",
+        "Keras",
+        "Numpy",
+        "Pandas",
+        "OpenCV",
+        "SciPy",
+        "Sklearn",
+        "Pillow",
+      ],
+      icon: "fa fa-cubes",
+    },
+    {
+      title: "web technologies",
+      info: ["React", "Node", "HTML", "CSS"],
+      icon: "fas fa-laptop-code",
+    },
+    {
+      title: "databases",
+      info: ["MongoDB", "MySQL", "OracleSQL"],
+      icon: "fa fa-database",
+    },
+    {
+      title: "operating systems & tools",
+      info: ["Ubuntu", "Windows", "Android", "Fedora", "Arch"],
+      icon: "fas fa-tools",
+    },
+    {
+      title: "design",
+      info: ["Illustrator", "Figma", "Canva"],
+      icon: "fa fa-pencil-square-o",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Quantum for Mars",
+      pictures: [
+        {
+          //img: require("./src/assets/portfolio/msr/one.png"),
+        },
+        {
+          //img: require("./src/assets/portfolio/msr/two.png"),
+        },
+        {
+          //img: require("./src/assets/portfolio/msr/three.png"),
+        },
+        {
+          //img: require("./src/assets/portfolio/msr/four.png"),
+        },
+      ],
+      technologies: [
+        "Quantum Machine Learning",
+        "Qiskit",
+        "Machine Learning",
+        "QCNN",
+        "QResnet",
+      ],
+      category: "Machine Learning",
+
+      github: "https://github.com/PromitSarker/Quantum_for-mars",
+      visit: "https://github.com/PromitSarker/Quantum_for-mars",
+      description:
+        "This project is a Quantum Machine Learning approach, and aims to assist Mars rovers in identifying Martian terrains for improved navigation and efficiency. Even though the work was conducted in an emulated environment, we achieved an accuracy exceeding 80%. ",
+    },
+    {
+      name: "BoiKoi : A Book Recommendation project",
+      pictures: [
+        {
+          //img: require("./src/assets/portfolio/noq/logo.png"),
+        },
+        {
+          //img: require("./src/assets/portfolio/noq/one.png"),
+        },
+        {
+          //img: require("./src/assets/portfolio/noq/two.png"),
+        },
+        {
+          //img: require("./src/assets/portfolio/noq/three.png"),
+        },
+        {
+          //img: require("./src/assets/portfolio/noq/four.png"),
+        },
+      ],
+      technologies: ["Python", "KNN"],
+      category: "Machine Learning",
+      github: "https://github.com/PromitSarker/BookRecomendation",
+      visit: "https://github.com/PromitSarker/BookRecomendation",
+      description:
+        "The project utilizes the K-Nearest Neighbors (KNN) algorithm to analyze user ratings and reading history, identifying similar users for personalized book recommendations. It aims to enhance the user experience by providing tailored suggestions and encouraging the exploration of new genres and authors according to the preferences of the reader. ",
+    },
+    {
+      name: "EasyPharma: Pharmacy Management System",
+      pictures: [
+        {
+          //img: require("./src/assets/portfolio/post/one.png"),
+        },
+        {
+          //img: require("./src/assets/portfolio/post/two.png"),
+        },
+      ],
+      technologies: ["HTML5", "CSS", "SQL", "PHP"],
+      category: "Web-Development",
+      github: "https://github.com/PromitSarker/EasyPharma",
+      visit: "https://github.com/PromitSarker/EasyPharma",
+      description:
+        "A management system for pharmacy owners and employees. Works as a all rounder management system.",
+    },
+    {
+      name: "rain Tumor Detection System",
+      pictures: [
+        {
+          //img: require("./src/assets/portfolio/suicide/suicide-analyzer.png"),
+        },
+        {
+          //img: require("./src/assets/portfolio/suicide/suicide2-original.png"),
+        },
+      ],
+      technologies: ["Python", "Machine Learning", "CNN", "Pandas"],
+      category: "Machine Learning",
+      github: "https://github.com/PromitSarker/Brain_Tumor_Detection_299",
+      visit: "https://github.com/PromitSarker/Brain_Tumor_Detection_299",
+      description:
+        "Detects the presence of brain tumors from Magnetic Resonance Imaging (MRI) images.",
+    },
+    {
+      name: "BasicSudoku",
+      pictures: [
+        {
+          //img: require("./src/assets/portfolio/smit/one.png"),
+        },
+        {
+          // img: require("./src/assets/portfolio/smit/two.png"),
+        },
+        {
+          // img: require("./src/assets/portfolio/smit/three.png"),
+        },
+        {
+          // img: require("./src/assets/portfolio/smit/four.png"),
+        },
+      ],
+      technologies: ["PyGame", "Python"],
+      category: "Game",
+      github: "https://github.com/PromitSarker/BasicSudoku",
+      visit: "https://github.com/PromitSarker/BasicSudoku",
+      description: "A Sudoku game, built with Python and PyGame",
+    },
+    {
+      name: "CaeserCipher Encrypter",
+      pictures: [
+        {
+          //   img: require("./src/assets/portfolio/clumpr/logo.png"),
+        },
+        {
+          // img: require("./src/assets/portfolio/clumpr/one.png"),
+        },
+        {
+          // img: require("./src/assets/portfolio/clumpr/two.png"),
+        },
+      ],
+      technologies: ["Python"],
+      category: "CMD Application",
+      github: "https://github.com/PromitSarker/CaeserCipher",
+      visit: "https://github.com/PromitSarker/CaeserCipher",
+      description: "A CMD based encrypter System",
+    },
+  ],
+};
+
+export default info;
